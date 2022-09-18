813948676587c7689aa276bb3deb0de584a5be8d
NattanSIlva <natanxboy@gmail.com>
2022-09-18 02:23:52 -0300

    Add: cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,20 @@
-import "./styles.css";;
+import { Card } from "../../components/card";
+import "./style.css";
 
 function Home() {
   return (
     // fragment //
-    <> 
+    <div className="container"> 
       <h1>Lista de presença</h1>
       <input type="text" name="nome" id="nome__input" placeholder="Digite seu nome..."/>
       <button type="button">Adicionar</button>
-    </>
+
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
 
