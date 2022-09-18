e3bf36d4ee94541597c5cd27ea57faac511505d4
NattanSilva <natanxboy@gmail.com>
2022-09-18 01:55:08 -0300

    Add: first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";;
+
+function Home() {
+  return (
+    // fragment //
+    <> 
+      <h1>Lista de presença</h1>
+      <input type="text" name="nome" id="nome__input" placeholder="Digite seu nome..."/>
+      <button type="button">Adicionar</button>
+    </>
+  )
+}
+
+export default Home
